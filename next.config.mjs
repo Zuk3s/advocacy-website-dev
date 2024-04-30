a16667451f01eee6887cdecc6ae23c49307f2bd3
felipe.macedo0829 <felipe.macedo0829@gmail.com>
2024-04-30 23:01:28 +0000

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    env: {
+        API_URL: '',
+        API_KEY: '',
+        API_TOKEN: '',
+        TelNumber: '',
+        Email: '',
+        Wpp: ''
+    }
+};
+
+export default nextConfig;
