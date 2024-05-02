0eec403811624f67e6861d0e3e1790999eaa234f
Zuk3s <felipemarcedo_@hotmail.com>
2024-05-01 23:52:41 -0300

    :sparkles: feat: nova estilização e prop className

--- src/components/NavBar/NavRoot.tsx
+++ src/components/NavBar/NavRoot.tsx
@@ -3,6 +3,7 @@ import { twMerge } from "tailwind-merge";
 
 interface NavRootProps {
   children: ReactNode;
+  className?: string;
 }
 
 export function NavRoot({ children, ...rest }: NavRootProps) {
@@ -10,7 +11,8 @@ export function NavRoot({ children, ...rest }: NavRootProps) {
     <nav
       {...rest}
       className={twMerge(
-        "bg-rosa-400 w-full p-5"
+        "bg-rosa-200 w-full py-4 sm:px-16 px-1 shadow-lg flex row-span-3 items-center",
+        rest.className
       )}
     >
       {children}
