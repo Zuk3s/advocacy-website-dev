db29dd279a976de076ec9fe2c9f6bcfee2250924
Zuk3s <felipemarcedo_@hotmail.com>
2024-05-01 23:57:05 -0300

    :recycle: refactor: adicionada a importação do NavGroup

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,10 +2,12 @@ import { NavLink } from "./NavLink";
 import { NavDropdown } from "./NavDropdown";
 import { NavLogo } from "./NavLogo";
 import { NavRoot } from "./NavRoot";
+import { NavGroup } from "./NavGroup";
 
 export const NavBar = {
   Link: NavLink,
   Dropdown: NavDropdown,
   Logo: NavLogo,
   Root: NavRoot,
+  Group: NavGroup,
 };
