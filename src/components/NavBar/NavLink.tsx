6a7b10715a504985dfeab8cfc4b567c195f47c43
Zuk3s <felipemarcedo_@hotmail.com>
2024-05-01 23:54:23 -0300

    :recycle: refactor: Novas funcionalidades de props e estilos

--- src/components/NavBar/NavLink.tsx
+++ src/components/NavBar/NavLink.tsx
@@ -1,14 +1,21 @@
 import Link from "next/link";
 import { ReactNode } from "react";
 
-export function NavLink({children, href, size="small"}: {
-    children: ReactNode,
-    href: string,
-    size: string
+export function NavLink({
+  children,
+  href,
+  size = "small",
+}: {
+  children: ReactNode;
+  href: string;
+  size?: string;
 }) {
-    return (
-        <Link href={href} className="font-bold text-lg p-2 hover:text-gray-900">
-            {children}
-        </Link>
-    );
+  return (
+    <Link
+      href={href}
+      className="text-base text-white font-medium px-5 py-2 hover:underline hover:underline-offset-4 decoration-2 decoration-dashed antialiased"
+    >
+      {children}
+    </Link>
+  );
 }
