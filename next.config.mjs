6b6ab9867fd494f0451670df39bedc0be6e6c8d7
Zuk3s <felipemarcedo_@hotmail.com>
2024-05-02 00:02:14 -0300

    :wrench: build: Adicionado o trailingSlash

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    env: {
-        API_URL: '',
-        API_KEY: '',
-        API_TOKEN: '',
-        TelNumber: '',
-        Email: '',
-        Wpp: ''
-    }
+  env: {
+    API_URL: "",
+    API_KEY: "",
+    API_TOKEN: "",
+    TelNumber: "",
+    Email: "",
+    Wpp: "",
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
