41df07696676b657dc0fd946b1e970e4960c825e
Zuk3s <felipemarcedo_@hotmail.com>
2024-05-03 02:17:14 -0300

    :sparkles: feat: novo componente Title para as páginas

---
+++ src/components/Content/Title.tsx
@@ -0,0 +1,7 @@
+interface TitleProps {
+  children: React.ReactNode;
+}
+
+export default function Title({ children }: TitleProps) {
+  return <h1 className="block title capitalize text-rosa-900 text-center mb-8 text-3xl font-semibold">{children}</h1>;
+}
