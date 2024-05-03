2f30daf7a39401bfbfdfc545476e5b31e49821fb
Zuk3s <felipemarcedo_@hotmail.com>
2024-05-03 02:16:41 -0300

    :recycle: refactor: removendo NavLogo

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,13 +1,11 @@
 import { NavLink } from "./NavLink";
 import { NavDropdown } from "./NavDropdown";
-import { NavLogo } from "./NavLogo";
 import { NavRoot } from "./NavRoot";
 import { NavGroup } from "./NavGroup";
 
-export const NavBar = {
+export const Nav = {
   Link: NavLink,
   Dropdown: NavDropdown,
-  Logo: NavLogo,
   Root: NavRoot,
   Group: NavGroup,
 };
