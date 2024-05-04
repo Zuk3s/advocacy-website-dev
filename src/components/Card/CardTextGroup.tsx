14882b14029bd7d97b7f4a0d3a3d4a865058e9a7
Zuk3s <felipemarcedo_@hotmail.com>
2024-05-04 00:16:10 -0300

    delete arquivos

--- src/components/Card/CardTextGroup.tsx
+++
@@ -1,7 +0,0 @@
-interface CardTextGroupProps {
-  children: React.ReactNode;
-}
-
-export function CardTextGroup({ children }: CardTextGroupProps) {
-  return <div className="p-6">{children}</div>;
-}
