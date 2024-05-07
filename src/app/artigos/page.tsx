152beee829863e6e7554abd12daf7feb347eaba5
Zuk3s <felipemarcedo_@hotmail.com>
2024-05-07 03:54:55 -0300

    refactor: redução de código com novo componente

--- src/app/artigos/page.tsx
+++ src/app/artigos/page.tsx
@@ -1,38 +1,21 @@
 import { Card } from "@/components/Card/index";
-import Nav from "@/components/NavBar/Nav";
-import Logo from "../../../public/images/logo-b.png";
-import React from "react";
 import Title from "@/components/Content/Title";
+import Cards from "@/components/Card/Card";
+import { Metadata } from "next";
+import { Section } from "@/components/Content/Section";
 
-export default function Artigo() {
+export const metadata: Metadata = {
+  title: "Artigos",
+  description: "Teste",
+};
+
+export default function Artigos() {
   return (
-    <React.Fragment>
-      <Nav />
-      <Card.GroupText>
-        <Title>Artigos</Title>
-        <Card.Group>
-          <Card.Root href="">
-            <Card.Img src={Logo} />
-            <Card.GroupText>
-              <Card.Title as="h1">
-                Artigo uahdauwhduwa adwawdwa dawdadawdaw dawdwdaw dawdw adwwad
-              </Card.Title>
-              <Card.Content>Texto</Card.Content>
-            </Card.GroupText>
-          </Card.Root>
-          <Card.Root href="">
-            <Card.Img src={Logo} />
-            <Card.GroupText>
-              <Card.Title as="h2">Artigo 2</Card.Title>
-              <Card.Content>
-                No crepúsculo dourado, a brisa suave acaricia a paisagem, num
-                instante fugaz, o tempo parece parar, revelando a miragem da
-                natureza em sua plenitude.
-              </Card.Content>
-            </Card.GroupText>
-          </Card.Root>
-        </Card.Group>
-      </Card.GroupText>
-    </React.Fragment>
+    <Section>
+      <Title>Artigos</Title>
+      <Card.Group className="items-center">
+        <Cards />
+      </Card.Group>
+    </Section>
   );
 }
