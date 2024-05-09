ea7f4436fab2dcc452b5e071eecfb9e55a1c7070
Zuk3s <felipemarcedo_@hotmail.com>
2024-05-09 02:57:12 -0300

    style: novas estilizações

--- src/components/Card/components/Root.tsx
+++ src/components/Card/components/Root.tsx
@@ -10,7 +10,7 @@ export function CardRoot({ children, href }: CardRootProps) {
   return (
     <Link
       href={href}
-      className="bg-creme rounded-md flex md:flex-col grow flex-1 min-w-80 md:max-w-sm md:hover:scale-105 transition-transform ease-in-out"
+      className="bg-white bg-opacity-45 border border-rosa-200 shadow-md rounded-md flex md:flex-col flex-[1_1_40%] min-w-80 md:hover:scale-105 transition-transform ease-in-out"
     >
       {children}
     </Link>
