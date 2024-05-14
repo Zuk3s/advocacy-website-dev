94c4181b742b2003fa6db8ef8f762ef81d647b82
Zuk3s <felipemarcedo_@hotmail.com>
2024-05-14 18:23:32 -0300

    arquivo deletado

--- src/components/Card/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function CardFooter({ children }: { children: React.ReactNode }) {
-  return (
-    <footer className="flex mt-auto justify-end">
-      <p className="text-xs opacity-80 ">Postado em: {children}</p>
-    </footer>
-  );
-}
--- src/components/Card/components/TextGroup.tsx
+++
@@ -1,9 +0,0 @@
-interface CardTextGroupProps {
-  children: React.ReactNode;
-}
-
-export function CardTextGroup({ children }: CardTextGroupProps) {
-  return (
-    <div className="p-5 md:pt-4 w-full flex flex-col gap-4 md:h-full">{children}</div>
-  );
-}
