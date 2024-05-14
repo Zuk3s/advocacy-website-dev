e1c1fc0660f41f266c35df0d0a72ca381ed0b0f1
Zuk3s <felipemarcedo_@hotmail.com>
2024-05-14 18:27:48 -0300

    feat: novas funcionalidades adicionadas, componentes de voltar, Icone e Link

---
+++ src/components/Buttons/components/Icon.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+
+export default function Icon({
+  children,
+  className,
+  href,
+  svg,
+  text,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  href: string;
+  svg?: string;
+  text?: string;
+}) {
+  return (
+    <Link
+      href={href}
+      className={twMerge(
+        "p-4 bg-slate-900 hover:bg-slate-800 rounded-full space-x-2",
+        className
+      )}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        className={twMerge("w-6 h-6", svg)}
+      >
+        {children}
+      </svg>
+      {text && <p className="md:sr-only text-white">{text}</p>}
+    </Link>
+  );
+}
---
+++ src/components/Buttons/index.tsx
@@ -0,0 +1,9 @@
+import Back from "./components/Back";
+import Icon from "./components/Icon";
+import Link from "./components/Link";
+
+export const Button = {
+  Link,
+  Back,
+  Icon,
+};
