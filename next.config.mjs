d92ef9e35e296d98edbcd1a140aa01baab7c6de5
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-01-21 14:16:17 -0300

    Update next.config.mjs
    remoed env tokens

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  env: {
-    API_URL: "",
-    API_KEY: "",
-    API_TOKEN: "",
-    NEXT_PUBLIC_NUMBER: "(61) 9.9936-1491",
-    NEXT_PUBLIC_EMAIL: "dalete.rocha@advdf.com.br",
-    NEXT_PUBLIC_WHATSZAPP: "https://wa.me/5561999361491?text=Olá%20gostaria%20de%20mais%20informações!",
-  },
   trailingSlash: true,
 };
 
