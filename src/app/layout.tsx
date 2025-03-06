89467b3df0d153940a06f7818918855476595d2f
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-03-06 18:03:10 -0300

    SEO: adicionando metadata ao head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,38 @@ import { twMerge } from "tailwind-merge";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Advogada Dálete",
+  metadataBase: new URL("https://advocacy-website-dev.vercel.app/"),
+  title: "Dálete Rocha - Advocacia",
   description: `Bem-vindo(a)! Somos um escritório de advocacia comprometido em oferecer soluções jurídicas personalizadas e eficazes para atender às suas necessidades. 
   Nosso objetivo é não apenas conquistar sua confiança, mas construir uma parceria sólida e duradoura, baseada em respeito e comprometimento.`,
+  keywords: ["advogada", "melhores advogados", "advogado", "advocacia"],
+  openGraph: {
+    title: "Dálete Rocha - Advocacia",
+    description: `Somos um escritório de advocacia comprometido em oferecer soluções jurídicas personalizadas e eficazes para atender às suas necessidades. 
+    Nosso objetivo é não apenas conquistar sua confiança, mas construir uma parceria sólida e duradoura, baseada em respeito e comprometimento.`,
+    url: "https://advocacy-website-dev.vercel.app/",
+    siteName: "Escritório de Advocacia - Dálete Rocha",
+    images: [
+      {
+        url: "/public/self.jpg",
+        height: 1200,
+        width: 630,
+        alt: "Advogados especialistas",
+      },
+    ],
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    }
+  },
+  alternates: {
+    canonical: "https://advocacy-website-dev.vercel.app/",
+  },
 };
 
 export default function RootLayout({
