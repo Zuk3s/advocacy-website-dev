97fed7799f06a43a31814bd77d4c7483d8e3cadb
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-03-06 18:57:21 -0300

    refactor: substituir DropButton por um ícone de seta na seção de conteúdo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import Hero from "@/features/Hero";
 import About from "@/features/About";
 import Actings from "@/features/Actings";
-import DropButton from "@/components/DropButton";
 import Footer from "@/features/Footer";
 import Contact from "@/features/Contat";
+import { Section } from "@/components/Content/Section";
+import { IconButton } from "@/components/IconButton/IconButton";
+import { ArrowDown } from "lucide-react";
 
 export default function Home() {
   return (
     <>
       <Hero />
 
-      <DropButton />
+      <Section className="h-[20vh] flex justify-center items-center">
+        <IconButton href="#actings" className="motion-reduce:animate-bounce">
+          <ArrowDown color="white" />
+        </IconButton>
+      </Section>
 
       <Actings />
 
