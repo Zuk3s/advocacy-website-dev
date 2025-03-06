f537c29a60061dc8ea7b49f0d7cc4263af34427a
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-03-06 19:03:55 -0300

    refactor: ajustar formatação da descrição e imagem no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,18 +8,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   metadataBase: new URL("https://advocacy-website-dev.vercel.app/"),
   title: "Dálete Rocha - Advocacia",
-  description: `Bem-vindo(a)! Somos um escritório de advocacia comprometido em oferecer soluções jurídicas personalizadas e eficazes para atender às suas necessidades. 
-  Nosso objetivo é não apenas conquistar sua confiança, mas construir uma parceria sólida e duradoura, baseada em respeito e comprometimento.`,
+  description: `Bem-vindo(a)! Somos um escritório de advocacia comprometido em oferecer soluções jurídicas personalizadas e eficazes para atender às suas necessidades.
+Nosso objetivo é não apenas conquistar sua confiança, mas construir uma parceria sólida e duradoura, baseada em respeito e comprometimento.`,
   keywords: ["advogada", "melhores advogados", "advogado", "advocacia"],
   openGraph: {
     title: "Dálete Rocha - Advocacia",
-    description: `Somos um escritório de advocacia comprometido em oferecer soluções jurídicas personalizadas e eficazes para atender às suas necessidades. 
-    Nosso objetivo é não apenas conquistar sua confiança, mas construir uma parceria sólida e duradoura, baseada em respeito e comprometimento.`,
+    description: `Somos um escritório de advocacia comprometido em oferecer soluções jurídicas personalizadas e eficazes para atender às suas necessidades.
+Nosso objetivo é não apenas conquistar sua confiança, mas construir uma parceria sólida e duradoura, baseada em respeito e comprometimento.`,
     url: "https://advocacy-website-dev.vercel.app/",
     siteName: "Escritório de Advocacia - Dálete Rocha",
     images: [
       {
-        url: "/public/self.jpg",
+        url: "/self.jpg",
         height: 1200,
         width: 630,
         alt: "Advogados especialistas",
